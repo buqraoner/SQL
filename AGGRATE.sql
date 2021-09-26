536450a6280e805fc6d2b8aacb45a4f152808314
Buqraoner <boletroy@gmail.com>
2021-09-27 00:51:54 +0300

    Ders içerikleri eklendi.

---
+++ AGGRATE.sql
@@ -0,0 +1,65 @@
+
+
+
+
+
+-- SELECT MAX(length) FROM film
+-- WHERE rental_Rate IN (0.99,2.99)
+
+--SELECT MAX(length)
+	--FROM film;
+--SELECT MAX(length), MIN(length), SUM(replacement_cost) FROM film
+
+
+--SELECT ROUND(AVG(length), 3) FROM film;
+
+--SELECT COUNT(*) FROM film;
+
+--COUNT,MIN,MAX,AVG,SUM
+
+
+--SELECT MIN(replacement_cost)	FROM film;
+
+--SELECT max(rental_rate)	FROM film;
+
+
+
+
+
+
+
+
+
+
+-- SELECT MAX(length) FROM film
+-- WHERE rental_Rate IN (0.99,2.99)
+
+--SELECT MAX(length)
+	--FROM film;
+--SELECT MAX(length), MIN(length), SUM(replacement_cost) FROM film
+
+
+--SELECT ROUND
+
+
+--SELECT * FROM country
+--İlk 5 vriyi gçmk için ofset
+--OFFSET 6
+--LIMIT 4;
+
+
+
+
+
+--  10 TANE VERİYİ
+
+--SELECT * FROM film
+--where rental_rate =4.99
+--ORDER BY length
+--LIMIT 10;
+
+
+--SELECT * FROM film
+--WHERE replacement_cost = 14.99 AND rental_rate = 0.99
+--ORDER BY length DESC
+--LIMIT 7;
---
+++ LIMITANDOFSET.sql
@@ -0,0 +1,44 @@
+SELECT * FROM actor
+WHERE first_name ='Penelope'
+ORDER BY last_name
+OFFSET 2
+LIMIT 1;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+--SELECT * FROM country
+--İlk 5 vriyi gçmk için ofset
+--OFFSET 6
+--LIMIT 4;
+
+
+
+
+
+--  10 TANE VERİYİ
+
+--SELECT * FROM film
+--where rental_rate =4.99
+--ORDER BY length
+--LIMIT 10;
+
+
+--SELECT * FROM film
+--WHERE replacement_cost = 14.99 AND rental_rate = 0.99
+--ORDER BY length DESC
+--LIMIT 7;
