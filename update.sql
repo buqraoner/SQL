39787ca867d1b57c8333b2d0767fa20e980319e0
Buğra Öner <boletroy@gmail.com>
2021-11-19 00:05:14 +0300

    Update

---
+++ update.sql
@@ -0,0 +1,12 @@
+-- UPDATE <table_name>
+--SET column1 =value1;
+	--column2 =value2;
+	 --
+   --Where condition;
+
+
+   UPDATE author
+   	SET	first_name='Emrah Safa',
+   		last_name='Gürkan',
+   		email='emrahsafa@hotmail.com',
+   		birthday ='1980-01-01';
